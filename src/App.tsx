27253b9b6c2dd6a97c14d69ef0c641497ca24282
thisara-medi <thisarabandara.medi@gmail.com>
2023-12-06 17:07:24 +0530

    fix app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -10,18 +10,6 @@ import {table1DataDemo,table2DataDemo} from  './Models/winnersLits'
 
 function App() {
 
-  const table1Data  = [
-    { column1: 'A1', column2: 'B1' },
-    { column1: 'A2', column2: 'B2' },
-    
-  ];
-
-  const table2Data = [
-    { column1: 'X1', column2: 'Y1' },
-    { column1: 'X2', column2: 'Y2' },
-    
-  ];
-
   return (
     <div className="App">
       <NavBar />
